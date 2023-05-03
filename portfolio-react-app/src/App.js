156d8610c53f08570e28669698edcfcf92075057
Paula da Silva <carolpaulag@gmail.com>
2023-05-03 13:12:28 +0000

    Create react app, add navbar, home, css, skills

---
+++ portfolio-react-app/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
